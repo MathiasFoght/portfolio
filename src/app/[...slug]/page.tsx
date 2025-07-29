b63ff7df4b39b330e9d4825b6aa47e59fb592083
Mathias Foght <mathiasfoght98@gmail.com>
2025-07-29 07:43:31 +0200

    catch all route added

---
+++ src/app/[...slug]/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import Link from 'next/link';
+import styles from './catchAll.module.css';
+
+export default function CatchAll() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <div className={styles.errorCode}>:(</div>
+                <p className={styles.description}>Page not found </p>
+                <Link href="/" className={styles.homeButton}>
+                    Back
+                </Link>
+            </div>
+        </div>
+    );
+}
